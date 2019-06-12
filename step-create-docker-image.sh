06b24a7e97dfc3808d9267d4a490ad5ef100dfb1
Simon Levesque <simon@foilen.com>
2019-06-11 21:38:36 -0400

    Add GuavaCache

--- step-create-docker-image.sh
+++ step-create-docker-image.sh
@@ -27,5 +27,6 @@ find $DOCKER_BUILD
 echo ----[ Build docker image ]----
 DOCKER_IMAGE=foilen-infra-ui:$VERSION
 docker build -t $DOCKER_IMAGE $DOCKER_BUILD
+docker tag $DOCKER_IMAGE foilen/$DOCKER_IMAGE
 
 rm -rf $DOCKER_BUILD
