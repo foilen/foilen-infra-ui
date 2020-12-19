d4cdd46831392992e87324e36f00224279478408
Simon Levesque <simon@foilen.com>
2020-12-19 12:16:12 -0500

    Update create scripts

--- create-public-release.sh
+++ create-public-release.sh
@@ -7,15 +7,17 @@ cd $RUN_PATH
 
 # Check params
 if [ $# -ne 1 ]
-	then
-		echo Usage: $0 version;
-    echo E.g: $0 0.1.0
-		echo Version is MAJOR.MINOR.BUGFIX
-		echo Latest version:
-		git describe --abbrev=0
-		exit 1;
+    then
+        echo Usage: $0 version;
+        echo E.g: $0 0.1.0
+        echo Version is MAJOR.MINOR.BUGFIX
+        echo Latest version:
+        git describe --abbrev=0
+        exit 1;
 fi
 
+# Set environment
+export LANG="C.UTF-8"
 export VERSION=$1
 
 ./step-update-copyrights.sh
