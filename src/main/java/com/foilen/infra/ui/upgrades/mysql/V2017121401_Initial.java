c571fdd569a5db57900671a2165769c4956f011d
Simon Levesque <simon@foilen.com>
2021-01-02 13:14:32 -0500

    Update copyrights

--- src/main/java/com/foilen/infra/ui/upgrades/mysql/V2017121401_Initial.java
+++ src/main/java/com/foilen/infra/ui/upgrades/mysql/V2017121401_Initial.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra UI
     https://github.com/foilen/foilen-infra-ui
-    Copyright (c) 2017-2021 Foilen (http://foilen.com)
+    Copyright (c) 2017-2021 Foilen (https://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
